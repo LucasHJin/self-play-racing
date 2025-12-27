032a85e04e7cc840bfd4d088de1dcb61037c01d3
Lucas Jin <kblazer20@gmail.com>
2025-12-27 12:43:45 -0500

    feat: function to parse arguments for running ppo

--- ppo.py
+++ ppo.py
@@ -22,3 +22,37 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.distributions.categorical import Categorical
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="PPO implementation")
+    
+    # simplified
+    parser.add_argument("--gym-id", type=str, default="CartPole-v1",
+        help="the id of the gym environment")
+    parser.add_argument("--learning-rate", type=float, default=2.5e-4,
+        help="the learning rate of the optimizer")
+    parser.add_argument("--seed", type=int, default=1,
+        help="seed of the experiment")
+    parser.add_argument("--total-timesteps", type=int, default=25000,
+        help="total timesteps of the experiments")
+    parser.add_argument("--torch-deterministic", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="if toggled, `torch.backends.cudnn.deterministic=False`")
+    parser.add_argument("--cuda", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
+        help="if toggled, cuda will be enabled by default")
+    parser.add_argument("--num-steps", type=int, default=128,
+        help="the number of steps to run in each environment per policy rollout")
+    parser.add_argument("--gamma", type=float, default=0.99,
+        help="the discount factor gamma")
+    parser.add_argument("--clip-coef", type=float, default=0.2,
+        help="the surrogate clipping coefficient")
+    parser.add_argument("--ent-coef", type=float, default=0.01,
+        help="coefficient of the entropy")
+    parser.add_argument("--vf-coef", type=float, default=0.5,
+        help="coefficient of the value function")
+    
+    # optimizations (TO BE DONE LATER)
+    
+    args = parser.parse_args()
+    args.batch_size = int(args.num_steps)
+    
+    return args
