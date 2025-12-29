8fd8e826ca2edab36e12786ac5474b30ee595f54
Lucas Jin <kblazer20@gmail.com>
2025-12-29 17:32:29 -0500

    feat: init and reset functions for car class

--- environment/car.py
+++ environment/car.py
@@ -1,11 +1,24 @@
 
 
 class Car:
-    def __init__(self):
-        pass
+    MAX_SPEED = 30.0
+    ACCELERATION = 10.0
+    STEERING_SPEED = 3.0
+    DRAG = 0.95
+    
+    def __init__(self, track):
+        self.track = track
+        self.reset()
     
     def reset(self):
-        pass
+        self.x, self.y, self.angle = self.track.get_start_pos()
+        self.vx = 0.0
+        self.vy = 0.0
+        self.angular_velocity = 0.0
+        self.progress = 0.0
+        self.checkpoints = 0
+        self.crashed = False
+        self.finished = False
     
     def update(self):
         pass
