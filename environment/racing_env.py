e635a980d21f962d4954ca6d70786b47861ea129
Lucas Jin <kblazer20@gmail.com>
2025-12-29 19:42:48 -0500

    feat: helper functions for dry

--- environment/racing_env.py
+++ environment/racing_env.py
@@ -3,6 +3,8 @@ import gymnasium as gym
 from track import Track
 from car import Car
 
+# https://gymnasium.farama.org/introduction/create_custom_env/
+
 class RacingEnv(gym.Env):
     def __init__(self):
         super().__init__()
@@ -26,11 +28,22 @@ class RacingEnv(gym.Env):
         ) 
         
     def _get_obs(self):
-        pass
+        return np.array([
+            self.car.vx,
+            self.car.vy,
+            self.car.angle,
+            self.car.angular_velocity,
+            self.car.progress
+        ], dtype=np.float32)
     
     def _get_info(self):
-        pass
-    
+        return {
+            "position": (self.car.x, self.car.y),
+            "speed": np.sqrt(self.car.vx ** 2 + self.car.vy ** 2),
+            "progress": self.car.progress,
+            "crashed": self.car.crashed
+        }
+        
     def reset(self, seed=None, options=None):
         pass
     
