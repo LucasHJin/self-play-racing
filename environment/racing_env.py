e0366088277d7fd59ae1bb6c6635c496c2e3d04f
Lucas Jin <kblazer20@gmail.com>
2025-12-29 19:23:34 -0500

    chore: setup structure for racing gym env

---
+++ environment/racing_env.py
@@ -0,0 +1,19 @@
+import numpy as np
+import gymnasium as gym
+
+class RacingEnv(gym.Env):
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def _get_obs(self):
+        pass
+    
+    def _get_info(self):
+        pass
+    
+    def reset(self, seed=None, options=None):
+        pass
+    
+    def step(self, action):
+        pass
