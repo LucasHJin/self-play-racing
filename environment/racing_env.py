398bb999f3ca08b99a5aeb41c109af5a8b4d79e1
Lucas Jin <kblazer20@gmail.com>
2025-12-29 22:00:21 -0500

    feat: updated reward shaping

--- environment/racing_env.py
+++ environment/racing_env.py
@@ -15,6 +15,7 @@ class RacingEnv(gym.Env):
         # for step function
         self.steps = 0
         self.last_progress = 0.0
+        self.half_passed = False
         
         # [steering, throttle]
         self.action_space = gym.spaces.Box(
@@ -55,6 +56,7 @@ class RacingEnv(gym.Env):
         self.car.reset()
         self.steps = 0
         self.last_progress = 0.0
+        self.half_passed = False
         
         observation = self._get_obs()
         info = self._get_info()
@@ -67,17 +69,31 @@ class RacingEnv(gym.Env):
         self.car.update(steering, throttle)
         self.steps += 1
         
+        # update checkpoint status
+        if not self.half_passed and self.car.progress >= 0.5:
+            self.half_passed = True
+        
         # reward shaping
+        lap_completed = False
         progress_delta = self.car.progress - self.last_progress
+        if self.half_passed and self.last_progress > 0.9 and self.car.progress < 0.1:
+            self.car.finished = True
+            lap_completed = True
+            progress_delta = (1.0 - self.last_progress) + self.car.progress  # fix delta across wrap
+            
         speed = np.sqrt(self.car.vx**2 + self.car.vy**2)
         reward = 0.0
-        reward += progress_delta * 20.0
-        reward += speed * 0.2
-        if self.last_progress > 0.9 and self.car.progress < 0.1:
-            reward += 50.0
-            self.car.finished = True
+        reward += progress_delta * 500.0
+        if lap_completed:
+            reward += 800.0
+        if progress_delta > 0:
+            reward += speed * 0.1
+        elif progress_delta < 0:
+            reward += progress_delta * 1000
         if self.car.crashed:
-            reward -= 100.0
+            reward -= 2000.0
+        if speed < 5.0:
+            reward -= 2.0
             
         # get returns
         observation = self._get_obs()
@@ -85,6 +101,7 @@ class RacingEnv(gym.Env):
         info["reward"] = reward
         info["progress_delta"] = progress_delta
         info["speed"] = speed
+        info["half_passed"] = self.half_passed
         terminated = self.car.crashed or self.car.finished
         truncated = self.steps >= 1000
         
