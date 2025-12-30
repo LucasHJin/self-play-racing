5295f3059e0dae46f1934c221c48678190d71c24
Lucas Jin <kblazer20@gmail.com>
2025-12-29 19:40:46 -0500

    feat: initialization of racing env with action and obs space defined

--- environment/racing_env.py
+++ environment/racing_env.py
@@ -1,11 +1,30 @@
 import numpy as np
 import gymnasium as gym
+from track import Track
+from car import Car
 
 class RacingEnv(gym.Env):
     def __init__(self):
         super().__init__()
-        pass
-    
+        
+        self.track = Track()
+        self.car = Car(self.track)
+        
+        # [steering, throttle]
+        self.action_space = gym.spaces.Box(
+            low=np.array([-1.0, 0.0]),
+            high=np.array([1.0, 1.0]),
+            shape=(2,),
+            dtype=np.float32
+        )
+        # [vx, vy, angle, angular_velocity, progress]
+        self.observation_space = gym.spaces.Box(
+            low=-np.inf,
+            high=np.inf,
+            shape=(5,),
+            dtype=np.float32
+        ) 
+        
     def _get_obs(self):
         pass
     
